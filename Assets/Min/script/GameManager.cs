d8dd2c1b7c3104a8d89035b8a3fede6052af6cf6
minhtet97 <minhtetnai97@gmail.com>
2022-08-22 23:37:06 +0900

    シン移動とゲームマネージャーの管理

---
+++ Assets/Min/script/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager manager;
+    int sceneNum;
+    [SerializeField]
+    private KeyCode KeyWord;
+
+    void Awake()
+    {
+        if(manager == null)
+        {
+            manager = this;
+            DontDestroyOnLoad(this);
+        }
+        else if(manager != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sceneNum = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyWord))
+        {
+            NextScene();
+        }
+    }
+    void NextScene()
+    {
+        /*sceneNum++;
+        if(sceneNum > 1)
+        {
+            sceneNum = 0;
+        }
+        SceneManager.LoadScene(sceneNum);*/
+        sceneNum = Random.Range(1, 3);
+        SceneManager.LoadScene(sceneNum);
+
+    }
+}
